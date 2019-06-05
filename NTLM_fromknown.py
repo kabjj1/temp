76a12468d1b56d42be40bcf6a73dd04162a4c270
kabjj1 <51392939+kabjj1@users.noreply.github.com>
2019-06-04 23:28:07 -0400

    Update NTLM_fromknown.py

--- NTLM_fromknown.py
+++ NTLM_fromknown.py
@@ -2,9 +2,9 @@
 
 import itertools as it,hashlib,binascii
 pw = 'password'
-per = list(it.combinations_with_replacement('12345$
+per = list(it.combinations_with_replacement('1234567890-=`!@#$%^&*()_+,./<>?;:"[]{}abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',3))
 for x in per:
-        t1 = ''.join(x)
-        intstr = pw + t1
-        ntlmhash = binascii.hexlify(hashlib.new('m$
-        print pw + t1 + "       " + ntlmhash 
+	t1 = ''.join(x)
+	intstr = pw + t1
+	ntlmhash = binascii.hexlify(hashlib.new('md4', intstr.encode('utf-16le')).digest())
+	print pw + t1 + "	" + ntlmhash 
